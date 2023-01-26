8deb05adfbf6fa12053709f8e4ed7aef00c7c9b9
Cristian Mariño <49408281+cristianemm96@users.noreply.github.com>
2023-01-26 18:43:29 -0300

    Create date.js

---
+++ src/Utils/date.js
@@ -0,0 +1,9 @@
+let date = new Date();
+export let getDate = ()=>{
+  String(date.getDate()).padStart(2, "0") +
+  "/" +
+  String(date.getMonth() + 1).padStart(2, "0") +
+  "/" +
+  date.getFullYear();
+}
+ 
